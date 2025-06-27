601942260b7074781f49017cb353aeb9a0f23776
PunsaraniJayawardhana <punsaranijayawardhana@gmail.com>
2025-06-27 22:13:49 +0530

    added sonar 6

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,4 @@
-import inputs from '../../auth_test/data/input';
+
 /// <reference types="cypress" />
 // ***********************************************
 // This example commands.ts shows you how to
@@ -41,9 +41,7 @@ import inputs from '../../auth_test/data/input';
 
 declare global {
   namespace Cypress {
-    interface Chainable {
-      login(username?: string, password?: string): Chainable<void>;
-    }
+   
   }
 }
 
@@ -58,41 +56,4 @@ declare global {
 
 // export {};
 
-Cypress.Commands.add('login', (username = inputs.correct_email, password = inputs.correct_password) => {
-  cy.visit('/login');
-
-  cy.get('[data-testid="Email Address"]').type(username);
-  cy.get('[data-testid="password"]').type(password);
-  cy.get('button[type="submit"]').click();
-
-  // Wait until accessToken is stored in localStorage
-  cy.window().then((win) => {
-    cy.log('Waiting for Cognito tokens...');
-    return new Cypress.Promise((resolve, reject) => {
-      const checkToken = () => {
-        const keys = Object.keys(win.localStorage);
-        const tokenKey = keys.find(
-          (key) => key.includes('CognitoIdentityServiceProvider') && key.endsWith('accessToken')
-        );
-        const token = tokenKey ? win.localStorage.getItem(tokenKey) : null;
-
-        if (token) {
-          cy.log('Cognito login complete');
-          resolve(token);
-        } else {
-          setTimeout(checkToken, 500); // Retry after 500ms
-        }
-      };
-      checkToken();
-    });
-  });
-
-  cy.window().then((win) => {
-  const accessTokenKey = Object.keys(win.localStorage).find((k) =>
-    k.includes('CognitoIdentityServiceProvider') && k.endsWith('accessToken')
-  );
-  expect(win.localStorage.getItem(accessTokenKey)).to.exist;
-});
-
-});
 export {};
